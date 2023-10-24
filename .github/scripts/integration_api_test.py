b2fdb92d3beec7cb244c4414c51cad72c6a91f6c
Aleksandr Iantsen <aleksandr.iantsen@zabbix.com>
2023-10-24 17:56:24 +0400

    added workflow with integration tests for Zabbix API

---
+++ .github/scripts/integration_api_test.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import sys
+import unittest
+
+sys.path.append('.')
+from zabbix_utils.api import ZabbixAPI, ZabbixAPIVersion
+
+
+class IntegrationAPITest(unittest.TestCase):
+    """Test working with a real Zabbix API instance"""
+
+    def setUp(self):
+        self.url = 'localhost'
+        self.user = 'Admin'
+        self.password = 'zabbix'
+        self.zapi = ZabbixAPI(
+            url=self.url,
+            user=self.user,
+            password=self.password,
+            skip_version_check=True
+        )
+
+    def tearDown(self):
+        if self.zapi:
+            self.zapi.logout()
+
+    def test_login(self):
+        """Tests login function works properly"""
+
+        self.assertEqual(
+            type(self.zapi), ZabbixAPI, "Login was going wrong")
+        self.assertEqual(
+            type(self.zapi.api_version()), ZabbixAPIVersion, "Version getting was going wrong")
+
+    def test_version_get(self):
+        """Tests getting version info works properly"""
+
+        version = None
+        if self.zapi:
+            version = self.zapi.apiinfo.version()
+        self.assertEqual(
+            version, str(self.zapi.api_version()), "Request apiinfo.version was going wrong")
+
+    def test_check_auth(self):
+        """Tests checking authentication state works properly"""
+
+        resp = None
+        if self.zapi:
+            if self.zapi.session_id == self.zapi.token:
+                resp = self.zapi.user.checkAuthentication(token=self.zapi.session_id)
+            else:
+                resp = self.zapi.user.checkAuthentication(sessionid=self.zapi.session_id)
+        self.assertEqual(
+            type(resp), dict, "Request user.checkAuthentication was going wrong")
+
+    def test_user_get(self):
+        """Tests getting users info works properly"""
+
+        users = None
+        if self.zapi:
+            users = self.zapi.user.get(
+                output=['userid', 'alias']
+            )
+        self.assertEqual(type(users), list, "Request user.get was going wrong")
+
+
+if __name__ == '__main__':
+    unittest.main()
