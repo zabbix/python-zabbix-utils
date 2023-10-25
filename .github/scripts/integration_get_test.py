7fa7190d93901fa0b80461e04b7c2fe829149ac9
Aleksandr Iantsen <aleksandr.iantsen@zabbix.com>
2023-10-25 13:28:21 +0400

    added workflow with integration tests for Zabbix get

---
+++ .github/scripts/integration_get_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+import json
+import unittest
+
+sys.path.append('.')
+from zabbix_utils.get import ZabbixGet
+
+
+class IntegrationGetTest(unittest.TestCase):
+    """Test working with a real Zabbix agent instance"""
+
+    def setUp(self):
+        self.host = '127.0.0.1'
+        self.port = 10050
+        self.agent = ZabbixGet(
+            host=self.host,
+            port=self.port
+        )
+
+    def test_get(self):
+        """Tests getting item values from Zabbix agent works properly"""
+
+        resp = self.agent.get('net.if.discovery')
+
+        self.assertIsNotNone(resp, "Getting item values was going wrong")
+        try:
+            resp_list = json.loads(resp)
+        except json.decoder.JSONDecodeError:
+            self.fail(f"raised unexpected Exception while parsing response: {resp}")
+
+        self.assertEqual(type(resp_list), list, "Getting item values was going wrong")
+        for resp in resp_list:
+            self.assertEqual(type(resp), dict, "Getting item values was going wrong")
+
+
+if __name__ == '__main__':
+    unittest.main()
